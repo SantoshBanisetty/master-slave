2f595d41ab3f463ab71fae8964c080b228854ea7
SantoshBanisetty <sb4438@nyu.edu>
2015-08-29 13:28:21 -0400

    First commit

---
+++ src/master.cpp
@@ -0,0 +1,100 @@
+/*
+ * master.cpp
+ *
+ *  Created on: Aug 27, 2015
+ *      Author: santosh
+ */
+
+//Master node should guide the master robot autonomously without bumping into
+//objects and should publish master robot's position every second(which will be
+//subscribed by the slave robot in the Slave node )
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
+#include <sstream>
+
+ros::Publisher velocity_publisher;
+void move(double speed, double distance, bool isForward);
+/**
+ * This tutorial demonstrates simple sending of messages over the ROS system.
+ */
+int main(int argc, char **argv)
+{
+  /**
+   * The ros::init() function needs to see argc and argv so that it can perform
+   * any ROS arguments and name remapping that were provided at the command line.
+   * For programmatic remappings you can use a different version of init() which takes
+   * remappings directly, but for most command-line programs, passing argc and argv is
+   * the easiest way to do it.  The third argument to init() is the name of the node.
+   *
+   * You must call one of the versions of ros::init() before using any other
+   * part of the ROS system.
+   */
+  ros::init(argc, argv, "master");
+
+  /**
+   * NodeHandle is the main access point to communications with the ROS system.
+   * The first NodeHandle constructed will fully initialize this node, and the last
+   * NodeHandle destructed will close down the node.
+   */
+  //ros::NodeHandle masterNode;
+  ros::NodeHandle n;
+  /**
+   * The advertise() function is how you tell ROS that you want to
+   * publish on a given topic name. This invokes a call to the ROS
+   * master node, which keeps a registry of who is publishing and who
+   * is subscribing. After this advertise() call is made, the master
+   * node will notify anyone who is trying to subscribe to this topic name,
+   * and they will in turn negotiate a peer-to-peer connection with this
+   * node.  advertise() returns a Publisher object which allows you to
+   * publish messages on that topic through a call to publish().  Once
+   * all copies of the returned Publisher object are destroyed, the topic
+   * will be automatically unadvertised.
+   *
+   * The second parameter to advertise() is the size of the message queue
+   * used for publishing messages.  If messages are published more quickly
+   * than we can send them, the number here specifies how many messages to
+   * buffer up before throwing some away.
+   */
+ // ros::Publisher master_pub = masterNode.advertise<std_msgs::String>("myPose", 1000);
+  velocity_publisher = n.advertise<geometry_msgs::Twist>("/robot_0/cmd_vel", 1000);
+  //ros::Rate loop_rate(10);
+while (ros::ok())
+{
+move (2.0, 5.0, 1);
+}
+ 
+
+
+  return 0;
+}
+
+void move(double speed, double distance, bool isForward){
+	geometry_msgs::Twist vel_msg;
+   //set a random linear velocity in the x-axis
+   if (isForward)
+	   vel_msg.linear.x =abs(speed);
+   else
+	   vel_msg.linear.x =-abs(speed);
+   vel_msg.linear.y =0;
+   vel_msg.linear.z =0;
+   //set a random angular velocity in the y-axis
+   vel_msg.angular.x = 0;
+   vel_msg.angular.y = 0;
+   vel_msg.angular.z =0;
+
+   double t0 = ros::Time::now().toSec();
+   double current_distance = 0.0;
+   ros::Rate loop_rate(100);
+   do{
+	   velocity_publisher.publish(vel_msg);
+	   double t1 = ros::Time::now().toSec();
+	   current_distance = speed * (t1-t0);
+	   ros::spinOnce();
+	   loop_rate.sleep();
+   }while(current_distance<distance);
+   vel_msg.linear.x =0;
+   velocity_publisher.publish(vel_msg);
+
+}
+
