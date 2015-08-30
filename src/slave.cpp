5f7216676fb329681539f17ae803b7a0013b909d
SantoshBanisetty <sb4438@nyu.edu>
2015-08-29 22:44:29 -0400

    3rd commit: (code cleaned) slave-move rotate distance deg2rad functions added

--- src/slave.cpp
+++ src/slave.cpp
@@ -10,62 +10,126 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "geometry_msgs/Twist.h"
+#include "sensor_msgs/LaserScan.h"
+#include "nav_msgs/Odometry.h"
+#include <sstream>
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-void goToGoal(const std_msgs::String::ConstPtr& msg)
-{
-  ROS_INFO("I heard: [%s]", msg->data.c_str());
-}
+#define PI 3.14
+ros::Publisher velocity_publisher;
+//Function declerations of move and rotate
+void move(double speed, double distance, bool isForward);
+void rotate (double angular_speed, double relative_angle, bool clockwise);
+//Function declearations for equilidian distance and degrees to radians conversion
+double getDistance(double x1, double y1, double x2, double y2);
+double degrees2radians(double angle_in_degrees);
+
+//void goToGoal(const std_msgs::String::ConstPtr& msg)
+//{
+//  ROS_INFO("I heard: [%s]", msg->data.c_str());
+//}
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+ 
   ros::init(argc, argv, "slave");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
   ros::NodeHandle slaveNode;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-  ros::Subscriber slave_sub = slaveNode.subscribe("myPose", 1000, goToGoal);
+  velocity_publisher = slaveNode.advertise<geometry_msgs::Twist>("/robot_1/cmd_vel", 1000);
+  //ros::Subscriber slave_sub = slaveNode.subscribe("myPose", 1000, goToGoal);
 
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
+// just for testing
+  while (ros::ok())
+  {
+  	move (2.0, 5.0, 1);
+  	rotate (2.0, 180.0, 1);
+  }
+ 
   ros::spin();
 
   return 0;
 }
 
+/**
+ *  makes the robot turn with a certain linear velocity, for 
+ *  a certain distance either forward or backward  
+ */
+void move(double speed, double distance, bool isForward){
+	geometry_msgs::Twist vel_msg;
+   //set a random linear velocity in the x-axis
+   if (isForward)
+	   vel_msg.linear.x =abs(speed);
+   else
+	   vel_msg.linear.x =-abs(speed);
+   vel_msg.linear.y =0;
+   vel_msg.linear.z =0;
+   //set a random angular velocity in the y-axis
+   vel_msg.angular.x = 0;
+   vel_msg.angular.y = 0;
+   vel_msg.angular.z =0;
 
+   double t0 = ros::Time::now().toSec();
+   double current_distance = 0.0;
+   ros::Rate loop_rate(100);
+   do{
+	   velocity_publisher.publish(vel_msg);
+	   double t1 = ros::Time::now().toSec();
+	   current_distance = speed * (t1-t0);
+	   ros::spinOnce();
+	   loop_rate.sleep();
+   }while(current_distance<distance);
+   vel_msg.linear.x =0;
+   velocity_publisher.publish(vel_msg);
 
+}
+
+/**
+ *  makes the robot turn with a certain angular velocity, for 
+ *  a certain distance in either clockwise or counter-clockwise direction  
+ */
+void rotate (double angular_speed, double relative_angle, bool clockwise){
+//angular_speed = degrees2radians(angular_speed);
+relative_angle = degrees2radians(relative_angle);
+	geometry_msgs::Twist vel_msg;
+	   //set a random linear velocity in the x-axis
+	   vel_msg.linear.x =0;
+	   vel_msg.linear.y =0;
+	   vel_msg.linear.z =0;
+	   //set a random angular velocity in the y-axis
+	   vel_msg.angular.x = 0;
+	   vel_msg.angular.y = 0;
+	   if (clockwise)
+	   		   vel_msg.angular.z =-abs(angular_speed);
+	   	   else
+	   		   vel_msg.angular.z =abs(angular_speed);
+
+	   double t0 = ros::Time::now().toSec();
+	   double current_angle = 0.0;
+	   ros::Rate loop_rate(1000);
+	   do{
+		   velocity_publisher.publish(vel_msg);
+		   double t1 = ros::Time::now().toSec();
+		   current_angle = angular_speed * (t1-t0);
+		   ros::spinOnce();
+		   loop_rate.sleep();
+		   //cout<<(t1-t0)<<", "<<current_angle <<", "<<relative_angle<<endl;
+	   }while(current_angle<relative_angle);
+	   vel_msg.angular.z =0;
+	   velocity_publisher.publish(vel_msg);
+}
+
+/**
+ *  converts angles from degree to radians  
+ */
+
+double degrees2radians(double angle_in_degrees){
+	return angle_in_degrees *PI /180.0;
+}
+
+/*
+ * get the euclidian distance between two points 
+ */
+double getDistance(double x1, double y1, double x2, double y2){
+	return sqrt(pow((x1-x2),2)+pow((y1-y2),2));
+}
